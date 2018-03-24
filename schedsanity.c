563a9951463e9a3ab79da58549d979b50bbbf2ab
shechter michael <shechtem@cs302row1-1.cs.bgu.ac.il>
2018-03-24 17:57:20 +0300

    final sanity check

--- schedsanity.c
+++ schedsanity.c
@@ -6,6 +6,7 @@
 #define LARGE_CALC_ITERATIONS 1000000
 #define MED__IO_ITERATIONS 100
 #define LARGE_IO_ITERATIONS 1000
+#define NUM_OF_ITERATIONS 10
 
 int
 medCalc()
@@ -51,47 +52,86 @@ int
 main(int argc, char *argv[])
 {
     int x=0;
-    int pid1,pid2,pid3,pid4;
+    int pid[4* NUM_OF_ITERATIONS];
     int wtime1,wtime2,wtime3,wtime4;
     int rtime1,rtime2,rtime3,rtime4;
     int iotime1,iotime2,iotime3,iotime4;
+
+    int avgwtime1=0,avgwtime2=0,avgwtime3=0,avgwtime4=0;
+    int avgrtime1=0,avgrtime2=0,avgrtime3=0,avgrtime4=0;
+    int avgiotime1=0,avgiotime2=0,avgiotime3=0,avgiotime4=0;
+
     
-    
-    pid1=fork();
-    if (pid1==0){
-        x=medCalc();
-        printf(1,"med calc finished: %d\n",x);
-        exit();
+    int i;
+    for(i = 0; i < NUM_OF_ITERATIONS ; i++){
+        set_priority(3);
+        pid[i*4]=fork();
+        if (pid[i*4]==0){
+            x=medCalc();
+            printf(1,"med calc finished: %d\n",x);
+            exit();
+        }
+        
+        pid[i*4 + 1]=fork();
+        if (pid[i*4 + 1]==0){
+            x=largeCalc();
+            printf(1,"large calc finished: %d\n",x);
+            exit();
+        }
+        
+        pid[i*4 + 2]=fork();
+        if (pid[i*4 + 2]==0){
+            medIo();
+            exit();
+        }
+        
+        set_priority(1);
+        pid[i*4 + 3]=fork();
+        if (pid[i*4 + 3]==0){
+            largeIo();
+            exit();
+        }
     }
-    
-    pid2=fork();
-    if (pid2==0){
-        x=largeCalc();
-        printf(1,"large calc finished: %d\n",x);
-        exit();
+
+    for(i = 0; i < NUM_OF_ITERATIONS ; i++){
+        wait2(pid[i*4],&wtime1,&rtime1,&iotime1);
+        avgwtime1 += wtime1;
+        avgrtime1 += rtime1;
+        avgiotime1 += iotime1;
+
+        wait2(pid[i*4 + 1],&wtime2,&rtime2,&iotime2);
+        avgwtime2 += wtime2;
+        avgrtime2 += rtime2;
+        avgiotime2 += iotime2;
+
+        wait2(pid[i*4 + 2],&wtime3,&rtime3,&iotime3);
+        avgwtime3 += wtime3;
+        avgrtime3 += rtime3;
+        avgiotime3 += iotime3;
+
+        wait2(pid[i*4 + 3],&wtime4,&rtime4,&iotime4);
+        avgwtime4 += wtime4;
+        avgrtime4 += rtime4;
+        avgiotime4 += iotime4;
     }
-    
-    pid3=fork();
-    if (pid3==0){
-        medIo();
-        exit();
-    }
-    
-    pid4=fork();
-    if (pid4==0){
-        largeIo();
-        exit();
-    }
-    
-    wait2(pid1,&wtime1,&rtime1,&iotime1);
-    wait2(pid2,&wtime2,&rtime2,&iotime2);
-    wait2(pid3,&wtime3,&rtime3,&iotime3);
-    wait2(pid4,&wtime4,&rtime4,&iotime4);
-    
-    printf(1,"med calc times: wtime: %d, rtime: %d, iotime: %d\n",wtime1,rtime1,iotime1);
-    printf(1,"large calc times: wtime: %d, rtime: %d, iotime: %d\n",wtime2,rtime2,iotime2);
-    printf(1,"med io times: wtime: %d, rtime: %d, iotime: %d\n",wtime3,rtime3,iotime3);
-    printf(1,"large io times: wtime: %d, rtime: %d, iotime: %d\n",wtime4,rtime4,iotime4);
+
+    avgwtime1 /= NUM_OF_ITERATIONS;
+    avgwtime2 /= NUM_OF_ITERATIONS;
+    avgwtime3 /= NUM_OF_ITERATIONS;
+    avgwtime4 /= NUM_OF_ITERATIONS;
+    avgrtime1 /= NUM_OF_ITERATIONS;
+    avgrtime2 /= NUM_OF_ITERATIONS;
+    avgrtime3 /= NUM_OF_ITERATIONS;
+    avgrtime4 /= NUM_OF_ITERATIONS;
+    avgiotime1 /= NUM_OF_ITERATIONS;
+    avgiotime2 /= NUM_OF_ITERATIONS;
+    avgiotime3 /= NUM_OF_ITERATIONS;
+    avgiotime4 /= NUM_OF_ITERATIONS;
+
+    printf(1,"med calc times: wtime: %d, rtime: %d, iotime: %d\n",avgwtime1,avgrtime1,avgiotime1);
+    printf(1,"large calc times: wtime: %d, rtime: %d, iotime: %d\n",avgwtime2,avgrtime2,avgiotime2);
+    printf(1,"med io times: wtime: %d, rtime: %d, iotime: %d\n",avgwtime3,avgrtime3,avgiotime3);
+    printf(1,"large io times: wtime: %d, rtime: %d, iotime: %d\n",avgwtime4,avgrtime4,avgiotime4);
     
     exit();
 }
